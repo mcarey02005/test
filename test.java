3e4bcbb0b04d9153baff1c7afe638979661eead0
mcarey02005 <mcarey02005@gmail.com>
2025-01-16 12:14:30 -0500

    comments

--- test.java
+++ test.java
@@ -3,3 +3,11 @@ public class test {
         System.out.println("hi");
     }
 }
+
+// git config --global user.name [name]
+// git config --global user.email [email]
+
+// git clone repoLink
+// git add .
+// git commit -m "test"
+// git push origin main      main is repo/branch in ur pc, we are pushing whats in main to origin.  git push . works after
